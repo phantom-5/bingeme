4d3ee59e96e5d859001af84a5e20f028fe245eaf
Rudra Madhav Biswal <35427300+phantom-5@users.noreply.github.com>
2022-08-11 07:28:12 +0530

    Update Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import '../App.css'
 const Loading = () => {
     return (
         <div className="loading">
-            <span className='badge-pill badge-dark' style={{'font-size':'1rem'}}>Fetching at {window.navigator.connection.downlink}&nbsp;Mbps...</span>
+            <span className='badge-pill badge-dark' style={{'font-size':'1rem'}}>Fetching...</span>
         </div>
     )
 }
